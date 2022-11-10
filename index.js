efa5529959bffb58bdc2b8a1013001916d5815b4
raf1n <rafinc10@gmail.com>
2022-11-10 18:36:14 +0600

    urlencode-middleware-added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 // middleware
 app.use(cors());
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Server running");
 });
